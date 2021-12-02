9d6e6c3b66c60784cd4e4f4330b412d79beb5e3b
xuwenjie <1054999089@qq.com>
2021-12-02 18:45:04 +0800

    占用内存top10的进程

---
+++ process_memory_use.sh
@@ -0,0 +1,30 @@
+# 统计系统中前十名使用内存最多的进程
+
+#! /bin/bash
+
+memory() {
+    # 创建临时文件，用于接收top内容
+    temp_file=`mktemp memory.XXX`
+    # 收集任务管理器信息
+    top -b -n1 > $temp_file
+    # 按进程统计内存使用大小
+    tail -n +8 $temp_file|awk '{array[$NF]+=$6}END{for (i in array)print array[i],i}'|sort -k 1 -n -r|head -10 
+
+    rm $temp_file
+}
+
+cpu() {
+    # 创建临时文件，用于接收top内容
+    temp_file=`mktemp cpu.XXX`
+    # 收集任务管理器信息
+    top -b -n1 > $temp_file
+    # 按进程统计内存使用大小
+    tail -n +8 $temp_file|awk '{array[$NF]+=$9}END{for (i in array)print array[i],i}'|sort -k 1 -n -r|head -10
+
+    rm $temp_file
+}
+echo "memory" "----------"
+memory
+
+echo "cpu" "-------------"
+cpu
