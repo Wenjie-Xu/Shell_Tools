62aaf0b31eb330ae02d5ae01a5ff4b9198c098ea
root <root@localhost.localdomain>
2021-12-09 19:15:49 +0800

    安装脚本

--- mysql_install.sh
+++ mysql_install.sh
@@ -4,6 +4,8 @@
 
 VERSION=57
 SOFTWARE_DIR=/home/xuwenjie/Softwares
+MYSQL_DATADIR_DIR=/usr/local/mysql/data
+MYSQL_DATADIR="\/usr\/local\/mysql\/data"
 MYSQL_REPO="mysql80-community-release-el7-3.noarch.rpm"
 
 # functions
@@ -13,14 +15,22 @@ check() {
         echo -e '\033[31m[ERROR]\033[0m Need root to execute~'
         exit 1
     fi
+
     # 2、检测是否有wget
     [ ! -x /usr/bin/wget ] && echo -e '\033[31m[ERROR]\033[0m Not find command /usr/bin/wget' && exit 1
+
+    # 3、安装依赖
+    if ! (yum -y install gcc-* pcre-devel zlib-devel elinks yum-utils 1> /dev/null);then
+	    echo "ERROR: yum install error"
+	    exit 1
+    fi
 }
 
 install_pre() {
-    # 1、检查是否有mysql对应版本的yum仓库
+    # 1、检查并安装mysql对应版本的yum仓库
     REPO_COUNT=`yum repolist all|grep -e "mysql$VERSION.*x86_64"|wc -l`
-    if [ $REPO_COUNT -lt 1 ];then # 如果没有mysql的源
+    # 如果没有mysql的源
+    if [ $REPO_COUNT -lt 1 ];then
         cd $SOFTWARE_DIR
         # 检查是否存在yum文件
         if [ ! -f $MYSQL_REPO ];then
@@ -31,27 +41,67 @@ install_pre() {
             fi
         fi
         # 下载成功，安装，否则报错
-        [ -f $MYSQL_REPO ] && rpm -ih $MYSQL_REPO &> /dev/null && echo -e "\033[32m[INFO]\033[0m Start install yum-repo..." || (echo -e "\033[31m[ERROR]\033[0m No file '$MYSQL_REPO', install fail" && exit 1)
+        [ -f $MYSQL_REPO ] && (echo -e "\033[32m[INFO]\033[0m Start install yum-repo..." && rpm -ih $MYSQL_REPO &> /dev/null) || (echo -e "\033[31m[ERROR]\033[0m No file '$MYSQL_REPO', install fail" && exit 1)
         REPO_COUNT=`yum repolist all|grep -e "mysql$VERSION.*x86_64"|wc -l`
     fi
     # 安装后检查
     [ $REPO_COUNT -eq 1 ] && echo -e "\033[32m[INFO]\033[0m Already get yum-repo :'$MYSQL_REPO'" || (echo -e "\033[31m[ERROR]\033[0m Still can not find yum-repo, Please check" && exit 1)
 
-    # 2、切换源版本
-    IS_ENABLE=`yum repolist all|grep -e "mysql$VERSION.*x86_64"|awk '{print $NF}'`
-    if [[ $IS_ENABLE == 'disable' ]];then
-        # awk找到enable的那个，将它用命令disbale掉，在enable对应版本（$1为源名）
-        echo 'hi'
+    # 2、切换指定源版本
+    echo -e "\033[32m[INFO]\033[0m Strat to checkout yum-version"
+    # 找到enable的，全部变成disabled
+    for NOW_ENABLE_VERSION in `yum repolist all|grep -e "mysql.*x86_64"|awk '{if($6=="enabled:"){print $1}}'|cut -d "/" -f1`
+        do
+            [[ ${NOW_ENABLE_VERSION: 0:1} == '!' ]] && NOW_ENABLE_VERSION= ${NOW_ENABLE_VERSION: 1}
+            if ! (yum-config-manager --disable $NOW_ENABLE_VERSION 1> /dev/null);then
+                echo -e '\033[31m[ERROR]\033[0m Yum config Fail,please check~'
+                exit 1
+            fi
+    done
+    # 将目标版本变成enable
+    TO_ENABLE_VERSION=`yum repolist all|grep -e "mysql$VERSION.*x86_64"|awk '{print $1}'|cut -d "/" -f1`
+    [[ ${TO_ENABLE_VERSION: 0:1} == '!' ]] && TO_ENABLE_VERSION= ${TO_ENABLE_VERSION: 1}
+    if ! (yum-config-manager --enable $TO_ENABLE_VERSION 1> /dev/null);then
+        echo -e '\033[31m[ERROR]\033[0m Yum config Fail,please check~'
+        exit 1
     fi
+    # 检查切换是否成功
+    IS_ENABLE=`yum repolist all|grep -e "mysql$VERSION.*x86_64"|awk '{print $6}'`
+    [[ $IS_ENABLE == 'enabled:' ]] && echo -e "\033[32m[INFO]\033[0m Prepare version:$VERSION success,ready to install" || (echo -e '\033[31m[ERROR]\033[0m Yum config Fail,please check~' && exit 1)
 }
 
 
 
 install() {
     # 使用yum安装mysql
-    echo 'hello'
-    # 检查是否有命令文件，有的话用systemd来重启、开启，关闭，list-units检查，否则报错检查
-    
+    echo -e "\033[32m[INFO]\033[0m Strat to install mysql..."
+    if ! (yum -y install mysql-community-server 1> /dev/null && yum -y install mysql-devel 1> /dev/null);then
+        echo -e '\033[31m[ERROR]\033[0m Yum install fail,please check~'
+        exit 1
+    fi
+    echo -e '\033[32m[ERROR]\033[0m Yum install success'
+    # 检查是否有命令文件，用systemd来启停检查
+    if [ -x `which mysql` ] && [ -f /etc/my.cnf ]
+        then
+        # 启动失败，修改配置
+        if ! (systemctl restart mysqld &> /dev/null && systemctl status mysqld &> /dev/null && systemctl stop mysqld &> /dev/null);then
+            # 修改配置文件
+            mkdir -p $MYSQL_DATADIR_DIR
+            if ! (sed -i.bnk -r "s/^(datadir).*/datadir=$MYSQL_DATADIR/" /etc/my.cnf);then echo -e '\033[31m[ERROR]\033[0m Mysql config error';exit 1;fi
+        fi
+        # 二次启动
+        if (systemctl restart mysqld &> /dev/null && systemctl status mysqld &> /dev/null && systemctl stop mysqld &> /dev/null);then
+            # systemd启动
+            TEMP_PASSWD=`grep 'temporary password' /var/log/mysqld.log|awk 'END{print $NF}'`
+            echo -e "\033[32m[INFO]\033[0m Mysql install and start success!"
+            echo "TEMP_PASSWD : $TEMP_PASSWD"
+        else
+            echo -e '\033[31m[ERROR]\033[0m Unknow mysql start error,please check' && exit 1
+        fi
+    else
+        echo -e '\033[31m[ERROR]\033[0m Cant find mysql config file,please check'
+        exit 1
+    fi
 }
 
 test() {
@@ -63,5 +113,4 @@ test() {
 
 # callback
 
-# check
-install_pre
+install
